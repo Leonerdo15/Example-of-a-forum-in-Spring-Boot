48ef2f0fbee4a7f5150da2da5a278461b755b4b6
Leonerdo15 <leonardolage10@gmail.com>
2021-10-03 22:02:24 +0100

    Initial commit

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -1,22 +1,31 @@
 package br.com.alura.forum.controller;
 
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.alura.forum.controller.Dto.TopicoDto;
-import br.com.alura.forum.modelo.Curso;
 import br.com.alura.forum.modelo.Topico;
+import br.com.alura.forum.repository.TopicoRepository;
 
 @RestController
 public class TopicosController {
     
+    @Autowired TopicoRepository topicoRepository;
+
     @RequestMapping("/topicos")
-    public List<TopicoDto> lista(){
-        Topico topico = new Topico("Duvida", "Duvida com Spring Boot", new Curso("Spring Boot", "Programaçao"));
-        return TopicoDto.converter(Arrays.asList(topico));
+    public List<TopicoDto> lista(String nomeCurso){
+       
+        if(nomeCurso == null){
+            List<Topico> topicos = topicoRepository.findAll();
+            return TopicoDto.converter(topicos);
+        }else{
+            List<Topico> topicos = topicoRepository.findByCursoNome(nomeCurso);
+            return TopicoDto.converter(topicos);
+        }
+        
 
     }
 }
